6a970f2289fc7d845611f03c9568a30a76db65d8
Ko'palov Muhammadjon <kupalovv.muhammadjon@gmail.com>
2024-06-18 14:44:19 +0500

    Mock data inserted

--- queries/language.sql
+++ queries/language.sql
@@ -4,3 +4,14 @@ create table languages (
     updated_at timestamp,
     deleted_at timestamp
 );
+INSERT INTO languages (name, created_at, updated_at, deleted_at)
+VALUES
+('Go', DEFAULT, NULL, NULL),
+('Python3', DEFAULT, NULL, NULL),
+('C', DEFAULT, NULL, NULL),
+('C++', DEFAULT, NULL, NULL),
+('Rust', DEFAULT, NULL, NULL),
+('Java', DEFAULT, NULL, NULL),
+('JavaScript', DEFAULT, NULL, NULL),
+('Kotlin', DEFAULT, NULL, NULL),
+('PHP', DEFAULT, NULL, NULL);
